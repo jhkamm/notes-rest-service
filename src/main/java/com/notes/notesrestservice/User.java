dab307a0940e150e897c82f9dbddf0836b13dc27
Jordan Kamm <jhkamm@gmail.com>
2022-01-21 13:34:36 -0600

    Add User.java

---
+++ src/main/java/com/notes/notesrestservice/User.java
@@ -0,0 +1,42 @@
+package com.notes.notesrestservice;
+
+import java.sql.Timestamp;
+
+public class User {
+    private String email;
+    private String password;
+    private Timestamp createTime;
+    private Timestamp lastUpdated;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Timestamp getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Timestamp createTime) {
+        this.createTime = createTime;
+    }
+
+    public Timestamp getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(Timestamp lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+}
